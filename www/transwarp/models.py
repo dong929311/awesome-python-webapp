5e9dda11f09da58e0e7e3811b53b1679345b08e6
dong <727248467@qq.com>
2016-10-16 14:44:23 +0800

    models

---
+++ www/transwarp/models.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+from db import next_int
+import time
+
+
+class User(Model):
+    __table__ = 'users'
+    id = StringField(primary_key=True, default=next_int, dd1='varchar(50)')
+    email = StringField(dd1='varchar(50)')
+    password = StringField(dd1='varchar(50)')
+    admin = BooleanField()
+    name = StringField(dd1='varchar(50)')
+    image = StringField(dd1='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    __table__ = 'blogs'
+    id = StringField(primary_key=True, default=next_int, dd1='varchar(50)')
+    user_id = StringField(dd1='varchar(50)')
+    user_name = StringField(dd1='varchar(50)')
+    name = StringField(dd1='varchar(50)')
+    summary = StringField(dd1='varchar(200)')
+    content = TextField(default='')
+    created_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+    __table__ = 'comments'
+    id = StringField(primary_key=True, default=next_int, dd1='varchar(50)')
+    blog_id = StringField(dd1='varchar(50)')
+    user_id = StringField(dd1='varchar(50)')
+    user_name = StringField(dd1='varchar(50)')
+    user_image = StringField(dd1='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
---
+++ www/transwarp/testdb.py
@@ -0,0 +1,12 @@
+import db
+import hashlib
+from models import User, Blog, Comment
+if __name__ == '__main__':
+    db.create_engine('root', 'root', 'awesome', host='192.168.56.104')
+    u = User(name='admin', email='admin@example.com', password='admin', image='about:blank')
+    u.update()
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    # u = User(name='Test', email='test@example.com', password='ttttt', image='about:blank').update()
+    user = User.findall(where='name=?', args=['Test'])
+    print user
