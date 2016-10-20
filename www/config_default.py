0cd845529cfcac4b5d3024e1f0c35bf789ab3820
dong <727248467@qq.com>
2016-10-20 15:50:28 +0800

    add config

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+
+configs = {
+    'db': {
+        'host': '192.168.56.104',
+        'port': '3306',
+        'user': 'root',
+        'password': 'root',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
